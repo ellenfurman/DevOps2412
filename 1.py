629f241ceb5dc208fe8d541fa69796e270b513ef
ellenfurman <ellenf205@gmail.com>
2024-02-18 19:01:23 +0200

    another commit

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-print("bla bla")
+
 # your_name = input("enter your name: ")
 a = "Ellen"
 b = "Furman"
@@ -8,7 +8,9 @@ Hobbies = ["bla1","bla2","bla3"]
 print(a + " " + b)
 about_me = [a, b, age, isMarried]
 about_me[2] = 28
-print(about_me)
-my_tuple = ("ellen", "furman") # cant change the value
+
+# my_tuple = ("ellen", "furman") # cant change the value
 
 # control / - is # for fwie lines
+
+print("ellen")
